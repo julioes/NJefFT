71aa96d54a3a21bf2a0a8eb0abcad585b2dad65a
Julio Espinoza-Sokal <julioes@gmail.com>
2022-01-06 21:27:28 -0500

    NFT v0 test

---
+++ tests/njefft_test.ts
@@ -0,0 +1,20 @@
+
+import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
+import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+
+Clarinet.test({
+    name: "Ensure that NFT token URL and ID is as expected",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const wallet_1 = accounts.get("wallet_1")!;
+
+        let block = chain.mineBlock([
+            Tx.contractCall("njefft", "get-last-token-id", [], wallet_1.address),
+            Tx.contractCall("njefft", "get-token-uri", [types.uint(1)], wallet_1.address)
+        ]);
+        assertEquals(block.receipts.length, 2);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectUint(0);
+        block.receipts[1].result.expectOk().expectSome().expectAscii("https://token.stacks.co/{id}.json");
+    },
+});
