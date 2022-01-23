23f695ce6500dadb4c8276cbf3072c04f444cb68
Julio Espinoza-Sokal <julioes@gmail.com>
2022-01-22 22:22:25 -0500

    Update NJefFT contract and tests

--- contracts/njefft.clar
+++ contracts/njefft.clar
@@ -1,27 +1,58 @@
 
 ;; njefft
-;; <add a description here>
+;; Jeff NFT collection
 
 ;; constants
-;;
+(define-constant contract-owner tx-sender)
+
+(define-constant err-owner-only (err u100))
+(define-constant err-mint-limit (err u101))
+(define-constant err-mint-disabled (err u102))
+(define-constant err-invalid-input (err u103))
 
 ;; data maps and vars
 ;;
+;; limit number of tokens
+(define-data-var mint-limit uint u1)
+;; Store the last issues token ID
+(define-data-var last-id uint u0)
+;; price of new tokens
+(define-data-var price uint u1000000)
+;; creator address
+(define-data-var creator-address principal 'ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5)
+;; metadata ipfs root cid
+(define-data-var ipfs-cid (string-ascii 80) "QmNrq2BQYA6JQEsP4PQg5XX7YSfeud5ikigi9MZmEV6W9w")
+;; minting paused
+(define-data-var mint-enabled bool false)
 
 ;; private functions
 ;;
-
-;; public functions
-;;
+;; Internal - Mint new NFT
+(define-private (mint (new-owner principal))
+    (let
+      (
+        (next-id (+ u1 (var-get last-id)))
+      )
+      (asserts! (<= next-id (var-get mint-limit)) err-mint-limit)
+      (asserts! (is-eq true (var-get mint-enabled)) err-mint-disabled)
+      (if (is-eq tx-sender contract-owner)
+        ;; contract owner no fee minting
+        true
+        (begin
+          ;; transfer stx
+          (try! (stx-transfer? (var-get price) tx-sender (var-get creator-address)))
+        )
+      )
+      (var-set last-id next-id)
+      (nft-mint? njefft next-id new-owner)))
 
 (impl-trait .njefft-trait.njefft-trait)
 
 ;; define a new NFT.
 (define-non-fungible-token njefft uint)
 
-;; Store the last issues token ID
-(define-data-var last-id uint u0)
-
+;; public functions
+;;
 ;; Claim a new NFT
 (define-public (claim)
   (mint tx-sender))
@@ -29,12 +60,10 @@
 ;; SIP009: Transfer token to a specified principal
 (define-public (transfer (token-id uint) (sender principal) (recipient principal))
   (begin
-     (asserts! (is-eq tx-sender sender) (err u403))
+     (asserts! (is-eq tx-sender sender) err-owner-only)
      ;; token-id and recipient are untrusted but nft-transfer will error out
      ;; #[allow(unchecked_data)]
-     (nft-transfer? njefft token-id sender recipient)
-  )
-)
+     (nft-transfer? njefft token-id sender recipient)))
 
 (define-public (transfer-memo (token-id uint) (sender principal) (recipient principal) (memo (buff 34)))
   (begin 
@@ -52,10 +81,42 @@
 
 ;; SIP009: Get the token URI. You can set it to any other URI
 (define-read-only (get-token-uri (token-id uint))
-  (ok (some "https://token.stacks.co/{id}.json")))
+  (ok (some (concat (concat "ipfs://ipfs/" (var-get ipfs-cid)) "/njefft-{id}-metadata.json"))))
 
-;; Internal - Mint new NFT
-(define-private (mint (new-owner principal))
-    (let ((next-id (+ u1 (var-get last-id))))
-      (var-set last-id next-id)
-      (nft-mint? njefft next-id new-owner)))
+(define-public (set-creator-address (address principal))
+  (begin
+    (asserts! (is-eq tx-sender contract-owner) err-owner-only)
+    (asserts! (< u0 (stx-get-balance address)) err-invalid-input) ;; how to validate a principal?
+    (ok (var-set creator-address address))))
+
+(define-public (set-price (new-price uint))
+  (begin
+    (asserts! (is-eq tx-sender contract-owner) err-owner-only)
+    (asserts! (< u0 new-price) err-invalid-input) ;; uint cant be negative anyway
+    (ok (var-set price new-price))))
+
+(define-public (toggle-enabled)
+  (begin
+    (asserts! (is-eq tx-sender contract-owner) err-owner-only)
+    (ok (var-set mint-enabled (not (var-get mint-enabled))))))
+
+(define-public (set-mint-limit (limit uint))
+  (begin
+    (asserts! (is-eq tx-sender contract-owner) err-owner-only)
+    (asserts! (> limit (var-get mint-limit)) err-mint-limit)
+    (ok (var-set mint-limit limit))))
+
+(define-public (set-ipfs-cid (cid (string-ascii 80)))
+  (begin
+    (asserts! (is-eq tx-sender contract-owner) err-owner-only)
+    (asserts! (<= u1 (len cid)) err-invalid-input)
+    (ok (var-set ipfs-cid cid))))
+  
+(define-read-only (get-mint-enabled)
+  (ok (var-get mint-enabled)))
+
+(define-read-only (get-price)
+  (ok (var-get price)))
+
+(define-read-only (get-mint-limit)
+  (ok (var-get mint-limit)))
--- tests/njefft_test.ts
+++ tests/njefft_test.ts
@@ -3,39 +3,360 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 // @ts-ignore
 import { assertEquals } from 'https://deno.land/std@0.122.0/testing/asserts.ts';
 
+const IPFS_ROOT = "QmNrq2BQYA6JQEsP4PQg5XX7YSfeud5ikigi9MZmEV6W9w"
+const ERR_OWNER_ONLY = 100
+const ERR_MINT_LIMIT = 101
+const ERR_MINT_DISABLED = 102
+const ERR_INVALID_INPUT = 103
+
 Clarinet.test({
     name: "Ensure that NFT token URL and ID is as expected",
     async fn(chain: Chain, accounts: Map<string, Account>) {
-        const wallet_1 = accounts.get("wallet_1")!;
+        const wallet_2 = accounts.get("wallet_2")!;
 
         let block = chain.mineBlock([
-            Tx.contractCall("njefft", "get-last-token-id", [], wallet_1.address),
-            Tx.contractCall("njefft", "get-token-uri", [types.uint(1)], wallet_1.address)
+            Tx.contractCall("njefft", "get-last-token-id", [], wallet_2.address),
+            Tx.contractCall("njefft", "get-token-uri", [types.uint(1)], wallet_2.address)
         ]);
         assertEquals(block.receipts.length, 2);
         assertEquals(block.height, 2);
 
         block.receipts[0].result.expectOk().expectUint(0);
-        block.receipts[1].result.expectOk().expectSome().expectAscii("https://token.stacks.co/{id}.json");
+        block.receipts[1].result.expectOk().expectSome().expectAscii(`ipfs://ipfs/${IPFS_ROOT}/njefft-{id}-metadata.json`);
     },
 });
 
 Clarinet.test({
-    name: "Ensure NFT can be transferred",
+    name: "By default NFT is not mintable on deployment",
     async fn(chain: Chain, accounts: Map<string, Account>) {
-        const wallet_1 = accounts.get("wallet_1")!;
+        const deployer = accounts.get("deployer")!;
         const wallet_2 = accounts.get("wallet_2")!;
 
         let block = chain.mineBlock([
-            Tx.contractCall("njefft", "claim", [], wallet_1.address),
-            Tx.contractCall("njefft", "get-last-token-id", [], wallet_1.address),
-            Tx.contractCall("njefft", "get-token-uri", [types.uint(1)], wallet_1.address)
+            Tx.contractCall("njefft", "get-mint-enabled", [], deployer.address),
+            Tx.contractCall("njefft", "claim", [], deployer.address),
+            Tx.contractCall("njefft", "get-last-token-id", [], wallet_2.address)
         ]);
         assertEquals(block.receipts.length, 3);
         assertEquals(block.height, 2);
 
-        block.receipts[1].result.expectOk().expectUint(1);
-        block.receipts[2].result.expectOk().expectSome().expectAscii("https://token.stacks.co/{id}.json");
+        block.receipts[0].result.expectOk().expectBool(false);
+        block.receipts[1].result.expectErr().expectUint(ERR_MINT_DISABLED);
+        block.receipts[2].result.expectOk().expectUint(0);
     },
 });
 
+Clarinet.test({
+    name: "NFT minting can be enabled by deployer",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get("deployer")!;
+        const wallet_2 = accounts.get("wallet_2")!;
+
+        let block = chain.mineBlock([
+            Tx.contractCall("njefft", "get-mint-enabled", [], deployer.address),
+            Tx.contractCall("njefft", "toggle-enabled", [], deployer.address),
+            Tx.contractCall("njefft", "get-mint-enabled", [], deployer.address),
+            Tx.contractCall("njefft", "claim", [], deployer.address),
+            Tx.contractCall("njefft", "get-last-token-id", [], wallet_2.address)
+        ]);
+        assertEquals(block.receipts.length, 5);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectBool(false);
+        block.receipts[1].result.expectOk().expectBool(true);
+        block.receipts[2].result.expectOk().expectBool(true);
+        block.receipts[3].result.expectOk().expectBool(true);
+        block.receipts[4].result.expectOk().expectUint(1);
+    },
+});
+
+Clarinet.test({
+    name: "NFT minting cannot be enabled by non-deployer",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const wallet_2 = accounts.get("wallet_2")!;
+
+        let block = chain.mineBlock([
+            Tx.contractCall("njefft", "get-mint-enabled", [], wallet_2.address),
+            Tx.contractCall("njefft", "toggle-enabled", [], wallet_2.address),
+            Tx.contractCall("njefft", "get-mint-enabled", [], wallet_2.address),
+            Tx.contractCall("njefft", "get-last-token-id", [], wallet_2.address)
+        ]);
+        assertEquals(block.receipts.length, 4);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectBool(false);
+        block.receipts[1].result.expectErr().expectUint(ERR_OWNER_ONLY);
+        block.receipts[2].result.expectOk().expectBool(false);
+        block.receipts[3].result.expectOk().expectUint(0);
+    },
+});
+
+Clarinet.test({
+    name: "NFT minting costs the price",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get("deployer")!;
+        const wallet_1 = accounts.get("wallet_1")!;
+        const wallet_2 = accounts.get("wallet_2")!;
+
+        let block = chain.mineBlock([
+            Tx.contractCall("njefft", "toggle-enabled", [], deployer.address),
+            Tx.contractCall("njefft", "get-price", [], deployer.address),
+            Tx.contractCall("njefft", "claim", [], wallet_2.address),
+            Tx.contractCall("njefft", "get-last-token-id", [], wallet_2.address),
+            Tx.contractCall("njefft", "get-owner", ["u1"], wallet_2.address)
+        ]);
+        assertEquals(block.receipts.length, 5);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectBool(true);
+        const price = Number(block.receipts[1].result.expectOk().expectUint(1_000_000));
+        block.receipts[2].result.expectOk().expectBool(true);
+        block.receipts[2].events.expectSTXTransferEvent(price, wallet_2.address, wallet_1.address)
+        block.receipts[3].result.expectOk().expectUint(1);
+        block.receipts[4].result.expectOk().expectSome().expectPrincipal(wallet_2.address);
+    },
+});
+
+Clarinet.test({
+    name: "Deployer can change the price",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get("deployer")!;
+        const wallet_1 = accounts.get("wallet_1")!;
+        const wallet_2 = accounts.get("wallet_2")!;
+
+        let block = chain.mineBlock([
+            Tx.contractCall("njefft", "toggle-enabled", [], deployer.address),
+            Tx.contractCall("njefft", "set-price", ["u2000000"], deployer.address),
+            Tx.contractCall("njefft", "claim", [], wallet_2.address),
+            Tx.contractCall("njefft", "get-last-token-id", [], wallet_2.address),
+            Tx.contractCall("njefft", "get-owner", ["u1"], wallet_2.address)
+        ]);
+        assertEquals(block.receipts.length, 5);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectBool(true);
+        block.receipts[1].result.expectOk().expectBool(true);
+        block.receipts[2].result.expectOk().expectBool(true);
+        block.receipts[2].events.expectSTXTransferEvent(2_000_000, wallet_2.address, wallet_1.address)
+        block.receipts[3].result.expectOk().expectUint(1);
+        block.receipts[4].result.expectOk().expectSome().expectPrincipal(wallet_2.address);
+    },
+});
+
+Clarinet.test({
+    name: "Price cannot be changed by non-deployer",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const wallet_2 = accounts.get("wallet_2")!;
+
+        let block = chain.mineBlock([
+            Tx.contractCall("njefft", "set-price", ["u2000000"], wallet_2.address),
+        ]);
+        assertEquals(block.receipts.length, 1);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectErr().expectUint(ERR_OWNER_ONLY);
+    },
+});
+
+Clarinet.test({
+    name: "Minting new tokens is limited by mint limit",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get("deployer")!;
+        const wallet_2 = accounts.get("wallet_2")!;
+
+        let block = chain.mineBlock([
+            Tx.contractCall("njefft", "toggle-enabled", [], deployer.address),
+            Tx.contractCall("njefft", "get-mint-limit", [], wallet_2.address),
+            Tx.contractCall("njefft", "claim", [], wallet_2.address),
+            Tx.contractCall("njefft", "claim", [], wallet_2.address),
+            Tx.contractCall("njefft", "get-last-token-id", [], wallet_2.address)
+        ]);
+        assertEquals(block.receipts.length, 5);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectBool(true);
+        block.receipts[1].result.expectOk().expectUint(1);
+        block.receipts[2].result.expectOk().expectBool(true);
+        block.receipts[3].result.expectErr().expectUint(ERR_MINT_LIMIT);
+        block.receipts[4].result.expectOk().expectUint(1);
+    },
+});
+
+Clarinet.test({
+    name: "Deployer can change mint limit",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get("deployer")!;
+        const wallet_2 = accounts.get("wallet_2")!;
+
+        let block = chain.mineBlock([
+            Tx.contractCall("njefft", "toggle-enabled", [], deployer.address),
+            Tx.contractCall("njefft", "get-mint-limit", [], wallet_2.address),
+            Tx.contractCall("njefft", "set-mint-limit", ["u2"], deployer.address),
+            Tx.contractCall("njefft", "get-mint-limit", [], wallet_2.address),
+            Tx.contractCall("njefft", "claim", [], wallet_2.address),
+            Tx.contractCall("njefft", "claim", [], wallet_2.address),
+            Tx.contractCall("njefft", "get-last-token-id", [], wallet_2.address)
+        ]);
+        assertEquals(block.receipts.length, 7);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectBool(true);
+        block.receipts[1].result.expectOk().expectUint(1);
+        block.receipts[2].result.expectOk().expectBool(true);
+        block.receipts[3].result.expectOk().expectUint(2);
+        block.receipts[4].result.expectOk().expectBool(true);
+        block.receipts[5].result.expectOk().expectBool(true);
+        block.receipts[6].result.expectOk().expectUint(2);
+    },
+});
+
+Clarinet.test({
+    name: "Non-deployer cannot change mint limit",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const wallet_2 = accounts.get("wallet_2")!;
+
+        let block = chain.mineBlock([
+            Tx.contractCall("njefft", "get-mint-limit", [], wallet_2.address),
+            Tx.contractCall("njefft", "set-mint-limit", ["u2"], wallet_2.address),
+            Tx.contractCall("njefft", "get-mint-limit", [], wallet_2.address)
+        ]);
+        assertEquals(block.receipts.length, 3);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectUint(1);
+        block.receipts[1].result.expectErr().expectUint(ERR_OWNER_ONLY);
+        block.receipts[2].result.expectOk().expectUint(1);
+    },
+});
+
+Clarinet.test({
+    name: "Deployer can change creator address",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get("deployer")!;
+        const wallet_1 = accounts.get("wallet_1")!;
+        const wallet_2 = accounts.get("wallet_2")!;
+        const wallet_3 = accounts.get("wallet_3")!;
+
+        let block = chain.mineBlock([
+            Tx.contractCall("njefft", "toggle-enabled", [], deployer.address),
+            Tx.contractCall("njefft", "set-creator-address", [`'${wallet_3.address}`], deployer.address),
+            Tx.contractCall("njefft", "claim", [], wallet_2.address),
+        ]);
+        assertEquals(block.receipts.length, 3);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectBool(true);
+        block.receipts[1].result.expectOk().expectBool(true);
+        block.receipts[2].events.expectSTXTransferEvent(1_000_000, wallet_2.address, wallet_3.address)
+        block.receipts[2].result.expectOk().expectBool(true);
+    },
+});
+
+Clarinet.test({
+    name: "Non-deployer cannot change creator address",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get("deployer")!;
+        const wallet_1 = accounts.get("wallet_1")!;
+        const wallet_2 = accounts.get("wallet_2")!;
+        const wallet_3 = accounts.get("wallet_3")!;
+
+        let block = chain.mineBlock([
+            Tx.contractCall("njefft", "set-creator-address", [`'${wallet_3.address}`], wallet_2.address)
+        ]);
+        assertEquals(block.receipts.length, 1);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectErr().expectUint(ERR_OWNER_ONLY);
+    },
+});
+
+Clarinet.test({
+    name: "Deployer can change ipfs cid",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get("deployer")!;
+        const wallet_2 = accounts.get("wallet_2")!;
+        const newcid = "newcid123"
+
+        let block = chain.mineBlock([
+            Tx.contractCall("njefft", "toggle-enabled", [], deployer.address),
+            Tx.contractCall("njefft", "set-ipfs-cid", [`"${newcid}"`], deployer.address),
+            Tx.contractCall("njefft", "claim", [], wallet_2.address),
+            Tx.contractCall("njefft", "get-token-uri", ["u1"], wallet_2.address)
+        ]);
+        assertEquals(block.receipts.length, 4);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectBool(true);
+        block.receipts[1].result.expectOk().expectBool(true);
+        block.receipts[2].result.expectOk().expectBool(true);
+        block.receipts[3].result.expectOk().expectSome().expectAscii(`ipfs://ipfs/${newcid}/njefft-{id}-metadata.json`);
+    },
+});
+
+Clarinet.test({
+    name: "Deployer cannot change to invalid ipfs cid",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get("deployer")!;
+        const wallet_2 = accounts.get("wallet_2")!;
+
+        let block = chain.mineBlock([
+            Tx.contractCall("njefft", "set-ipfs-cid", [`""`], deployer.address),
+            Tx.contractCall("njefft", "get-token-uri", ["u1"], wallet_2.address)
+        ]);
+        assertEquals(block.receipts.length, 2);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectErr().expectUint(ERR_INVALID_INPUT);
+        block.receipts[1].result.expectOk().expectSome().expectAscii(`ipfs://ipfs/${IPFS_ROOT}/njefft-{id}-metadata.json`);
+    },
+});
+
+Clarinet.test({
+    name: "Minted NFT can be transferred",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get("deployer")!;
+        const wallet_2 = accounts.get("wallet_2")!;
+        const wallet_3 = accounts.get("wallet_3")!;
+
+        let block = chain.mineBlock([
+            Tx.contractCall("njefft", "toggle-enabled", [], deployer.address),
+            Tx.contractCall("njefft", "claim", [], wallet_2.address),
+            Tx.contractCall("njefft", "get-owner", ["u1"], wallet_2.address),
+            Tx.contractCall("njefft", "transfer", ["u1", `'${wallet_2.address}`, `'${wallet_3.address}`], wallet_2.address),
+            Tx.contractCall("njefft", "get-owner", ["u1"], wallet_2.address)
+        ]);
+        assertEquals(block.receipts.length, 5);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectBool(true);
+        block.receipts[1].result.expectOk().expectBool(true);
+        block.receipts[2].result.expectOk().expectSome().expectPrincipal(wallet_2.address);
+        block.receipts[3].result.expectOk().expectBool(true);
+        block.receipts[4].result.expectOk().expectSome().expectPrincipal(wallet_3.address);
+    },
+});
+
+Clarinet.test({
+    name: "Minted NFT can be transferred with memo",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get("deployer")!;
+        const wallet_2 = accounts.get("wallet_2")!;
+        const wallet_3 = accounts.get("wallet_3")!;
+
+        let block = chain.mineBlock([
+            Tx.contractCall("njefft", "toggle-enabled", [], deployer.address),
+            Tx.contractCall("njefft", "claim", [], wallet_2.address),
+            Tx.contractCall("njefft", "get-owner", ["u1"], wallet_2.address),
+            Tx.contractCall("njefft", "transfer-memo", ["u1", `'${wallet_2.address}`, `'${wallet_3.address}`, "0x676767"], wallet_2.address),
+            Tx.contractCall("njefft", "get-owner", ["u1"], wallet_2.address)
+        ]);
+        assertEquals(block.receipts.length, 5);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectBool(true);
+        block.receipts[1].result.expectOk().expectBool(true);
+        block.receipts[2].result.expectOk().expectSome().expectPrincipal(wallet_2.address);
+        block.receipts[3].result.expectOk().expectBool(true);
+        block.receipts[4].result.expectOk().expectSome().expectPrincipal(wallet_3.address);
+    },
+});
